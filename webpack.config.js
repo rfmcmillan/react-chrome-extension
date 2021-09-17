8f03373f4cb018dab9cec3ca40cf4d213f2b39b9
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-09-17 07:06:39 -0400

    added css-loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,10 @@ const HtmlPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   devtool: 'cheap-module-source-map',
-  entry: { popup: path.resolve('src/popup/popup.tsx') },
+  entry: {
+    popup: path.resolve('src/popup/popup.tsx'),
+    options: path.resolve('src/options/options.tsx'),
+  },
   module: {
     rules: [
       {
@@ -12,13 +15,21 @@ module.exports = {
         test: /\.tsx?$/,
         exclude: /node_modules/,
       },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/i,
+      },
+      {
+        type: 'asset/resource',
+        test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
+      },
     ],
   },
   plugins: [
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve('src/manifest.json'),
+          from: path.resolve('src/static'),
           to: path.resolve('dist'),
         },
       ],
@@ -28,6 +39,7 @@ module.exports = {
       filename: 'popup.html',
       chunks: ['popup'],
     }),
+    ...getHtmlPlugins(['popup', 'options']),
   ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
@@ -36,4 +48,20 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
 };
+
+function getHtmlPlugins(chunks) {
+  return chunks.map(
+    (chunk) =>
+      new HtmlPlugin({
+        title: 'React Extension',
+        filename: `${chunk}.html`,
+        chunks: [chunk],
+      })
+  );
+}
