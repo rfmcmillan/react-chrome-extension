334adc8d8fb0e671de488221ec9f8b506108f350
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-09-17 10:15:31 -0400

    added webpack cleanup plugin

---
+++ src/contentScript/contentScript.ts
@@ -0,0 +1,4 @@
+console.log("contentScript running!");
+chrome.runtime.sendMessage("From the content script!", (response) => {
+  console.log(response)
+});
--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -7,3 +7,4 @@ const test = <img src='icon.png' />
 const root = document.createElement('div')
 document.body.appendChild(root)
 ReactDOM.render(test, root)
+
